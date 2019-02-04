4e325f6f5bd7dba0d36df4d3855d6e5945f6ca8f
Rachel Wesley <email@rachwes94@gmail.com>
2019-02-04 12:01:51 -0500

    feat: Added a second CatClicker

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-let numOfClicks = 0;
+let numOfClicksCat1 = 0;
+let numOfClicksCat2 = 0;
+const label1 = 'Cat 1';
+const label2 = 'Cat 2';
+const labelBox1 = $('.labelBox1');
+const labelBox2 = $('.labelBox2');
 
-$('img').click(function(e) {
-  numOfClicks++;
-  $('h1').html(`Clicks: ${numOfClicks}`);
+labelBox1.html(`${label1}`);
+labelBox2.html(`${label2}`);
+$('.cat1label').html(`Clicks: 0`);
+$('.cat2label').html(`Clicks: 0`);
+
+
+$('.cat1').click(function(e) {
+  numOfClicksCat1++;
+  $('.cat1label').html(`Clicks: ${numOfClicksCat1}`);
+});
+
+$('.cat2').click(function(e) {
+  numOfClicksCat2++;
+  $('.cat2label').html(`Clicks: ${numOfClicksCat2}`);
 });
