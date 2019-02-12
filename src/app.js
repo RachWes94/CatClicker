d6cc2583c19375920e039607ce0baffa4b71a14b
Rachel Wesley <email@rachwes94@gmail.com>
2019-02-12 15:34:19 -0500

    feat: Updated using the MVC design pattern

--- src/app.js
+++ src/app.js
@@ -1,74 +1,111 @@
-// Start up function
-// Places cat names in a list
-function init() {
-  document.body.innerHTML = '';
-  const header = document.createElement('header');
-  document.body.appendChild(header);
-  // Let's loop over the numbers in our array
-  for (var i = 0; i < kitties.length; i++) {
 
-      var cat = kitties[i];
-
-      var elem = document.createElement('div');
-      elem.classList.add('nameList');
-      elem.textContent = cat.name;
-      // event listener on cat names to display the information
-      elem.addEventListener('click', (function(catCopy) {
-          return function() {
-            catCopy.display();
-          };
-      })(cat));
-
-      header.appendChild(elem);
-  };
-  // Create elements to hold cat info
-  const displayContainer = document.createElement('main');
-  const nameContainer = document.createElement('h1');
-  const imageContainer = document.createElement('div')
-  imageContainer.classList.add('imageContainer');
-  const image = document.createElement('img');
-  const clickContainer = document.createElement('p');
-  document.body.appendChild(displayContainer);
-  displayContainer.appendChild(nameContainer);
-  displayContainer.appendChild(imageContainer);
-  imageContainer.appendChild(image);
-  displayContainer.appendChild(clickContainer);
+// MODEL
+const model = {
+  // creating the initial cats
+  cats: {
+    'Cat 1': { name: 'Cat 1', src: './img/cat1.jpg', clicks: 0 },
+    'Cat 2': { name: 'Cat 2', src: './img/cat2.jpg', clicks: 0 },
+    'Cat 3': { name: 'Cat 3', src: './img/cat3.jpg', clicks: 0 },
+    'Cat 4': { name: 'Cat 4', src: './img/cat4.jpg', clicks: 0 },
+    'Cat 5': { name: 'Cat 5', src: './img/cat5.jpg', clicks: 0 }
+  },
+  // returns the 5 cat objects in cats
+  getAllCats: function() {
+    return Object.values(this.cats);
+  },
+  // returns specific cat
+  getCat: function(catTitle) {
+    return this.cats[catTitle];
+  },
+  // add 1 to the clicks of specific cat
+  incrementClicks: function(catTitle) {
+    this.cats[catTitle].clicks++;
+  }
 };
 
-// Creates the cats
-class Cat {
-  constructor(name, src) {
-    this.name = name;
-    this.src = src;
-    this.clicks = 0;
+
+// CONTROLLER
+const controller = {
+  // displays the header
+  init: function() {
+    headerView.render();
+  },
+  // returns all cats using the model
+  getAllCats: function() {
+    return model.getAllCats();
+  },
+  // returns specific cat using the model
+  getCat: function(catTitle) {
+    return model.getCat(catTitle);
+  },
+  // increments the clicks for the specific cat in the model
+  incrementClicks: function(catTitle) {
+    model.incrementClicks(catTitle);
+  },
+  // returns clicks specific to the cat
+  getCatClicks: function(catTitle) {
+    return model.getCat(catTitle).clicks;
   }
-  // removes old image if there is one and adds new
-  display() {
-    const previousImage = document.querySelector('img');
-    if (previousImage) { previousImage.remove() };
+}
 
-    const newImage = document.createElement('img');
-    document.querySelector('.imageContainer').appendChild(newImage);
+// VIEWS
+const headerView = {
+  // displays the cat names on page as clickable items
+  render: function() {
+    const cats = controller.getAllCats();
+    const nameList = document.querySelector('.nameList');
+    // loops through each cat of cats
+    // creates buttons with the cat.name and appends them to the header
+    // adds event listeners on all buttons to render the displayView
+    for (let cat of cats) {
+        const nameListItem = document.createElement('button');
+        nameListItem.textContent = cat.name;
+        nameList.appendChild(nameListItem);
+        nameListItem.addEventListener('click', function() {
+          // gets the specific cat in the controler and render the displayView
+          // based on that cat
+          displayView.render(controller.getCat(cat.name));
+        });
+    }
 
-    $('h1').html(this.name);
-    $('p').html(`Clicks: ${this.clicks}`);
-    // Adding event listener to the new image to count clicks
-    newImage.src = this.src;
-    newImage.addEventListener('click', () => {
-      this.clicks++;
-      $('p').html(`Clicks: ${this.clicks}`);
+  }
+}
+
+const displayView = {
+  render: function(cat) {
+    // grabbing the display element and clearing it before rendering incase there
+    // was a previous cat rendered
+    const displayBox = document.querySelector('.display');
+    displayBox.innerHTML = '';
+    // creating html elements to hold display info
+    const title = document.createElement('h1');
+    const image = document.createElement('img');
+    const clicker = document.createElement('p');
+    // loading the image asynchronously
+    // We want to ensure that the title and click counter are only set after
+    // the image has loaded.
+    new Promise(function(res, rej) {
+      image.onload = res;
+      image.onerror = rej;
+      image.src = cat.src;
+    }).then(function() {
+      // once the image is loaded, we will then set the title and clicker text
+      title.textContent = cat.name;
+      clicker.textContent = `Clicks: ${cat.clicks}`;
+      // appending title, image, and clicker to the display
+      displayBox.appendChild(title);
+      displayBox.appendChild(image);
+      displayBox.appendChild(clicker);
+      // adding click event to each cat picture to increment clicks
+      image.addEventListener('click', function() {
+        // increment clicks in controler
+        controller.incrementClicks(cat.name);
+        // updating clicks texts with each click
+        const updatedClicks = controller.getCatClicks(cat.name);
+        clicker.textContent = `Clicks: ${updatedClicks}`;
+      });
     });
   }
-};
+}
 
-// Creating the 5 cats
-const kitties = [
-  new Cat('Cat 1', './img/cat1.jpg'),
-  new Cat('Cat 2', './img/cat2.jpg'),
-  new Cat('Cat 3', './img/cat3.jpg'),
-  new Cat('Cat 4', './img/cat4.jpg'),
-  new Cat('Cat 5', './img/cat5.jpg')
-];
-
-// Calls the initial function to start the page
-init();
+controller.init();
