433b169efb0f055222513bf0bc90ade53ba5650c
Rachel Wesley <email@rachwes94@gmail.com>
2019-02-13 13:26:39 -0500

    feat: Added an admin feature

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 To practice basic DOM manipulation features and the MVC design pattern.
 
 ## How To Use
-Click on a Cat's name and the picture will display with the name and how many times that cat has been clicked. Click away!
+Click on a Cat's name and the picture will display with the name and how many times that cat has been clicked. The user can also us the admin button to display a form to edit the current cats information themselves. 
--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
+// adding event listener to the admin Button to toggle the admin form
+const adminButton = document.querySelector('#adminButton');
+const adminForm = document.querySelector('#adminForm');
 
 // MODEL
 const model = {
   // creating the initial cats
   cats: {
-    'Cat 1': { name: 'Cat 1', src: './img/cat1.jpg', clicks: 0 },
-    'Cat 2': { name: 'Cat 2', src: './img/cat2.jpg', clicks: 0 },
-    'Cat 3': { name: 'Cat 3', src: './img/cat3.jpg', clicks: 0 },
-    'Cat 4': { name: 'Cat 4', src: './img/cat4.jpg', clicks: 0 },
-    'Cat 5': { name: 'Cat 5', src: './img/cat5.jpg', clicks: 0 }
+    1: { id: 1, name: 'Cat 1', src: './img/cat1.jpg', clicks: 0 },
+    2: { id: 2, name: 'Cat 2', src: './img/cat2.jpg', clicks: 0 },
+    3: { id: 3, name: 'Cat 3', src: './img/cat3.jpg', clicks: 0 },
+    4: { id: 4, name: 'Cat 4', src: './img/cat4.jpg', clicks: 0 },
+    5: { id: 5, name: 'Cat 5', src: './img/cat5.jpg', clicks: 0 }
   },
   // returns the 5 cat objects in cats
   getAllCats: function() {
     return Object.values(this.cats);
   },
-  // returns specific cat
-  getCat: function(catTitle) {
-    return this.cats[catTitle];
+  // returns specific cat object
+  getCat: function(catId) {
+    return this.cats[catId];
+  },
+  // updates the cat object with new admin input information if there was an input
+  updateCat: function(catId, updatedCatObj) {
+    const prevCatObj = this.cats[catId];
+    this.cats[catId].name = updatedCatObj.name || prevCatObj.name;
+    this.cats[catId].src = updatedCatObj.src || prevCatObj.src;
+    this.cats[catId].clicks = updatedCatObj.clicks || prevCatObj.clicks;
   },
   // add 1 to the clicks of specific cat
-  incrementClicks: function(catTitle) {
-    this.cats[catTitle].clicks++;
+  incrementClicks: function(catId) {
+    this.cats[catId].clicks++;
   }
 };
 
@@ -29,22 +39,52 @@ const controller = {
   // displays the header
   init: function() {
     headerView.render();
+
+    // grabbing input DOM element and storing them in an object
+    const nameInput = document.querySelector("input[name='name']");
+    const urlInput = document.querySelector("input[name='url']");
+    const clickInput = document.querySelector("input[name='numClicks']");
+
+    adminButton.addEventListener('click', function() {
+      // grabbing current cat id and the object
+      const currentCatId = document.querySelector('.display')['data-id'];
+      const currentCat = model.getCat(currentCatId);
+      // Setting the default inputs to the current cat object
+      nameInput.value = currentCat.name;
+      urlInput.value = currentCat.src;
+      clickInput.value = currentCat.clicks;
+      // form will toggle on click
+      adminForm.classList.toggle('hidden');
+
+    });
+    adminForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      const inputObject = {name: nameInput.value, src: urlInput.value, clicks: clickInput.value};
+      // grabbing object with id of currently displayed cat
+      const currentCatId = document.querySelector('.display')['data-id'];
+      // updating cat information through the model and redrawing the cat display
+      model.updateCat(currentCatId, inputObject);
+      displayView.render(model.getCat(currentCatId));
+      headerView.render();
+
+    });
   },
   // returns all cats using the model
   getAllCats: function() {
     return model.getAllCats();
   },
   // returns specific cat using the model
-  getCat: function(catTitle) {
-    return model.getCat(catTitle);
+  getCat: function(catId) {
+    return model.getCat(catId);
   },
   // increments the clicks for the specific cat in the model
-  incrementClicks: function(catTitle) {
-    model.incrementClicks(catTitle);
+  incrementClicks: function(catId) {
+    model.incrementClicks(catId);
   },
   // returns clicks specific to the cat
-  getCatClicks: function(catTitle) {
-    return model.getCat(catTitle).clicks;
+  getCatClicks: function(catId) {
+    return model.getCat(catId).clicks;
   }
 }
 
@@ -52,8 +92,12 @@ const controller = {
 const headerView = {
   // displays the cat names on page as clickable items
   render: function() {
+    const header = document.querySelector('header');
+    header.innerHTML = '';
     const cats = controller.getAllCats();
     const nameList = document.querySelector('.nameList');
+    const adminButton = document.querySelector('#adminButton');
+    const adminForm = document.querySelector('#adminForm');
     // loops through each cat of cats
     // creates buttons with the cat.name and appends them to the header
     // adds event listeners on all buttons to render the displayView
@@ -64,10 +108,15 @@ const headerView = {
         nameListItem.addEventListener('click', function() {
           // gets the specific cat in the controler and render the displayView
           // based on that cat
-          displayView.render(controller.getCat(cat.name));
+          displayView.render(controller.getCat(cat.id));
+          // admin button is visible after display is drawn
+          adminButton.classList.remove('hidden');
+          // if the admin form is open is will be hidden
+          if (!adminForm.classList.contains('hidden')) {
+            adminForm.classList.add('hidden');
+          }
         });
     }
-
   }
 }
 
@@ -77,6 +126,7 @@ const displayView = {
     // was a previous cat rendered
     const displayBox = document.querySelector('.display');
     displayBox.innerHTML = '';
+    displayBox['data-id'] = cat.id;
     // creating html elements to hold display info
     const title = document.createElement('h1');
     const image = document.createElement('img');
@@ -88,6 +138,7 @@ const displayView = {
       image.onload = res;
       image.onerror = rej;
       image.src = cat.src;
+      image.alt = 'cat image';
     }).then(function() {
       // once the image is loaded, we will then set the title and clicker text
       title.textContent = cat.name;
@@ -98,10 +149,14 @@ const displayView = {
       displayBox.appendChild(clicker);
       // adding click event to each cat picture to increment clicks
       image.addEventListener('click', function() {
-        // increment clicks in controler
-        controller.incrementClicks(cat.name);
+        // if the form is open is will close
+        if (!adminForm.classList.contains('hidden')) {
+          adminForm.classList.add('hidden');
+        }
+        // increment clicks in controler for specfic cat
+        controller.incrementClicks(cat.id);
         // updating clicks texts with each click
-        const updatedClicks = controller.getCatClicks(cat.name);
+        const updatedClicks = controller.getCatClicks(cat.id);
         clicker.textContent = `Clicks: ${updatedClicks}`;
       });
     });
